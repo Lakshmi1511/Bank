557d9cda8cbae976fe62892c1a54cd5fbb6ce4b0
unknown <lakshmimohan90@gmail.com>
2023-05-04 17:56:43 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component,OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+   data="Your perfect banking partner"
+   placeHolderData="enter acno"
+
+   uname:any
+   psw:any
+
+  constructor(){ }
+  ngOnInit(): void {
+     
+  }
+  login(){
+    console.log("this.uname,this.psw");
+    //alert(this.ds.sdata)
+    //alert(this.ds.checkData())
+  }
+  unameChange(event:any){
+  console.log(event.target.value);
+  }
+}
